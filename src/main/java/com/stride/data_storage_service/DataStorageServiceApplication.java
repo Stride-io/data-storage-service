4487034d8c4bcb7fce3a93204de2a29899693346
psawhney <psawhney@vmware.com>
2024-07-03 18:12:56 -0700

    Initial commit

---
+++ src/main/java/com/stride/data_storage_service/DataStorageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stride.data_storage_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStorageServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStorageServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stride/data_storage_service/DataStorageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stride.data_storage_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStorageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
